af4ed904c2653f5c41b8125f5adbced0e140fdd4
notoriousgtw <notoriousgtw@gmail.com>
2018-06-13 13:25:37 -0700

    Update Current Hours Fix Intra.user.js

--- Current Hours Fix Intra.user.js
+++ Current Hours Fix Intra.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Current Hours Fix Intra
+// @name         hours_script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
